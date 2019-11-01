4ee2e41295ecd1fb2670e7593cb55a0959549728
Danets <dantesvv@gmail.com>
2019-11-01 12:34:58 +0200

    update

--- src/app/shared/add-form.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AddFormDirective } from './add-form.directive';
-
-describe('AddFormDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AddFormDirective();
-    expect(directive).toBeTruthy();
-  });
-});
